aae72b0b72ebc67cb308a0552b63d1d0ee51d78d
Erick Rodriguez <erickrod54@gmail.com>
2022-07-02 23:41:50 -0400

    comfy-sloth-ecommerce app version 1 - index js file - Features: Wrapping App using ProductsProvider in order to provide products data and features

--- src/index.js
+++ src/index.js
@@ -8,4 +8,21 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+/**comfy-sloth-ecommerce app version 1 - index js file
+ * - Features: 
+ * 
+ *      --> Wrapping 'App' using 'ProductsProvider'
+ *          in order to provide products data and 
+ *          features.
+ * 
+ * Notes: this is going to be use to implement the sidebar
+ * confitional checkout
+*/
+
+ReactDOM.render(
+<ProductsProvider>
+    <App />
+</ProductsProvider>, 
+
+document.getElementById('root')
+)
