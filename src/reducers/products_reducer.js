961147ef71b81c0088c773907c23e1c5e5fa2558
Erick Rodriguez <erickrod54@gmail.com>
2022-07-03 02:55:48 -0400

    comfy-sloth-ecommerce app version 2 - products_reducer file - Features: Building action.type: SIDEBAR_OPEN, Building action.type: SIDEBAR_CLOSE

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -9,7 +9,30 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
+/**comfy-sloth-ecommerce app version 2 - products_reducer 
+ * file - Features: 
+ * 
+ *      --> Building 'action.type: SIDEBAR_OPEN'.
+ * 
+ *      --> Building 'action.type: SIDEBAR_CLOSE'.
+ * 
+ * Notes: These actions will handle the toggle feature 
+ * for the 'Sidebar' Component.
+*/
+
 const products_reducer = (state, action) => {
+  
+  /**i spread the state, and add the state value
+   * that changes*/
+  if (action.type === SIDEBAR_OPEN) {
+      return {...state, isSidebarOpen: true}
+  }
+  
+  /**i spread the state, and add the state value
+   * that changes*/
+  if (action.type === SIDEBAR_CLOSE) {
+    return {...state, isSidebarOpen: false}
+}
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
