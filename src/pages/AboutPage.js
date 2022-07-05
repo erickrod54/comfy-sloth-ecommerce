02a9b35e9029cefd0234752b4741b94a2cf45774
Erick Rodriguez <erickrod54@gmail.com>
2022-07-04 22:22:18 -0400

    comfy-sloth-ecommerce app version 2 - About Page - Features: Building About page, Importing and Placing PageHero Component, Styling the Component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,8 +3,49 @@ import styled from 'styled-components'
 import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
+/**comfy-sloth-ecommerce app version 2 - 'About' Page
+ * - Features: 
+ * 
+ *      --> Building 'About' Component.
+ * 
+ *      --> Importing and Placing 'PageHero' Component.
+ * 
+ *      --> Styling the component.
+ * 
+ * Notes: Wrapper, title and underline gives styles to the
+ * 'AboutPage' Component.
+*/
+
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return(
+    <main>
+      <PageHero title='about'/>
+        <Wrapper className='page section section-center'>
+           <img src={aboutImg} alt='nice desk'/> 
+           <article>
+            <div className='title'>
+              <h2>our story</h2>
+              <div className='underline'></div>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit, sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. Nibh mauris 
+              cursus mattis molestie a iaculis at erat. Dolor 
+              magna eget est lorem ipsum dolor. Pretium vulputate
+             sapien nec sagittis aliquam malesuada bibendum arcu. 
+             Vitae aliquet nec ullamcorper sit. Varius duis at 
+             consectetur lorem donec. Commodo quis imperdiet massa 
+             tincidunt nunc. Id porta nibh venenatis cras 
+             sed felis. Tempus imperdiet nulla malesuada 
+             pellentesque elit eget gravida. Leo urna molestie 
+             at elementum eu facilisis sed odio morbi. 
+             Purus viverra accumsan in nisl nisi scelerisque. 
+             Praesent tristique magna sit amet.
+              Feugiat pretium nibh ipsum consequat nisl vel.</p>
+           </article>
+        </Wrapper>
+    </main>
+    )
 }
 
 const Wrapper = styled.section`
