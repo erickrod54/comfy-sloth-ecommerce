80a590436347f59f0159e027e67aa45f40a7b218
Erick Rodriguez <erickrod54@gmail.com>
2022-07-04 22:20:13 -0400

    comfy-sloth-ecommerce app version 2 - Error Page - Features: Building Error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+
+/**comfy-sloth-ecommerce app version 2 - Error Page
+ * - Features: 
+ * 
+ *      --> Bulding the 'Error' page
+ * 
+ * Notes: The 'Wrapper 'and 'btn' gives styles to
+ * the error page.
+*/
+
 const ErrorPage = () => {
-  return <h4>error page</h4>
+  return(
+    <Wrapper className='page-100'>
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page cannot be found</h3>  
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  
+     )
 }
 
 const Wrapper = styled.main`
