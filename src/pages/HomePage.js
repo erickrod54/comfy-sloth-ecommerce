e5ec546d925676414e793ee6b3e7a5d8665f8000
Erick Rodriguez <erickrod54@gmail.com>
2022-07-05 11:44:41 -0400

    comfy-sloth-ecommerce app version 2 - HomePage page - features: Building basic Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
+
+/**comfy-sloth-ecommerce app version 2 - 'HomePage'
+ * Component - Features: 
+ * 
+ *      --> Building basic 'Home' page.
+ * 
+ * Notes: A good practice in order to build a page 
+ * effectively is to place the Components that are
+ * going to be part of the page, for this case 
+ * i import' FeaturedProducts', 'Hero', 'Services',
+ * 'Contact' and place it 
+*/
+
 const HomePage = () => {
-  return <h4>home page</h4>
+  return(
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  )
 }
 
 export default HomePage
