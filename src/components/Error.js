a3de45e7f28b395a0a3fbb8b8c22c7f87bd9b283
Erick Rodriguez <erickrod54@gmail.com>
2022-07-07 02:02:17 -0400

    comfy-sloth-ecommerce app version 4 - Error Component - Features: Styling the Component and placing an error message -this component was placed on FeaturedProducts Component-

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,25 @@
 import React from 'react'
+
+/**comfy-sloth-ecommerce app version 4 - Error 
+ * Component - Features: 
+ * 
+ *      -->Styling the Component and placing
+ *        an error message.           
+ * 
+ * Notes: This Component was previously placed
+ * on 'FeaturedProducts' to handle the error
+ * when the data is not being obtained.
+ * 
+ * this is going to be tested on the 'prodcuts_contex'
+ *  > useEffect > url
+*/
+
 const Error = () => {
-  return <h4>error element</h4>
+  return(
+    <div className='section section-center text-center'>
+      <h2>there was an error...</h2>
+    </div>
+  )
 }
 
 export default Error
