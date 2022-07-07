6070aefa2931654b09f16c0ff385c5a9672e4f88
Erick Rodriguez <erickrod54@gmail.com>
2022-07-07 01:46:23 -0400

    comfy-sloth-ecommerce app version 4 - products_reducer file - Features: Building GET_PRODUCTS_SUCCESS action, Building GET_PRODUCTS_ERROR action

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -9,15 +9,22 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
-/**comfy-sloth-ecommerce app version 2 - products_reducer 
+/**comfy-sloth-ecommerce app version 4 - products_reducer 
  * file - Features: 
  * 
- *      --> Building 'action.type: SIDEBAR_OPEN'.
+ *      --> Building 'GET_PRODUCTS_SUCCESS' action.
  * 
- *      --> Building 'action.type: SIDEBAR_CLOSE'.
+ *      --> Building 'GET_PRODUCTS_SUCCESS' action.
  * 
- * Notes: These actions will handle the toggle feature 
- * for the 'Sidebar' Component.
+ * Notes: for the 'action.type' 'GET_PRODUCTS_SUCCESS'
+ * this is going to get me all the products, but
+ * i'll filter by 'featured' prop on 'true'.
+ * 
+ * As i sue filter i'm creating a completely
+ * new array - featured_products -
+ * 
+ * For c i just change the state 
+ * of the props to display the error.
 */
 
 const products_reducer = (state, action) => {
@@ -33,7 +40,29 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return {...state, isSidebarOpen: false}
 }
-  return state
+if (action.type === GET_PRODUCTS_BEGIN) {
+  return { ...state, products_loading: true }
+}
+
+if (action.type === GET_PRODUCTS_SUCCESS ) {
+  /**for featured products, i filter the products
+   * with the 'featured' prop 'true'*/
+  const featured_products = action.payload.filter(
+    (product) => product.featured === true )
+
+  return {...state,
+           products_loading: false,
+           products:action.payload, 
+          featured_products }
+}
+
+/**Fot the error i deactivate the 'loading' and
+ * change the 'products_error' state to 'true'*/
+if (action.type === GET_PRODUCTS_ERROR ) {
+  return {...state, products_loading:false, 
+    products_error: true}
+}
+  
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
