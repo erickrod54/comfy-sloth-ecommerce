7d5b4a207915be6ee8ecb839cb66f06e5278b1de
Erick Rodriguez <erickrod54@gmail.com>
2022-07-07 02:00:01 -0400

    comfy-sloth-ecommerce app version 4 - Product Component - Features: Destructuring image, name, price, and id - i have access throght Products because i proviusly map on this Component on FeaturedProducts Component-

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,8 +4,37 @@ import { formatPrice } from '../utils/helpers'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-const Product = () => {
-  return <h4>product</h4>
+/**comfy-sloth-ecommerce app version 4 - Product 
+ * Component - Features: 
+ * 
+ *      --> Destrucuring 'image', 'name', 'price',
+ *          and 'id'.             
+ * 
+ * Notes: I can destructure these 'image', 'name', 
+ * 'price', and 'id' props because they're being 
+ * mapped previuosly on 'FeaturedProducts' Component
+ * so i have access to them.
+ * 
+ * I build the 'link' with the 'id' on the icon
+ *  '<FaSearch />' and as 'footer' i place the
+ * rest of the props to describe the product
+*/
+
+const Product = ({ image, name, price, id }) => {
+  return(
+    <Wrapper>
+      <div className='container'>
+        <img src={image} alt={name}/>
+        <Link to={`/products/${id}`} className='link'>
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{price}</p>
+      </footer>
+    </Wrapper>
+    )
 }
 
 const Wrapper = styled.article`
