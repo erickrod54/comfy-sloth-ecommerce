706b025f537dd1b380056be7b62b82788df0fad0
Erick Rodriguez <erickrod54@gmail.com>
2022-07-12 11:40:34 -0400

    comfy-sloth-ecommerce app version 10 - AddToCart Component - Features: Destructuring product and getting his props, Building state to hanlde the colors values, Mapping the colors array and setting up in a button, Setting an active class to indicate that a color has being selected by the user

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,62 @@ import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+/**comfy-sloth-ecommerce app version 10 - AddToCart 
+ * file - Features: 
+ * 
+ *      --> Destructuring 'product' and getting 
+ *          his props. 
+ *      
+ *      --> Building state to handle the colors 
+ *          value.
+ * 
+ *      --> Mapping the 'colors' array an setting 
+ *          up in a 'button' 
+ * 
+ *      --> Setting an active class to indicate that 
+ *          a color have been selected by an user.                   
+ * 
+ * Notes: This version is the basic setup of the 
+ * 'AddToCart', part of the full functionallity is
+ * going to be provided by the 'cart_context' -this
+ * way the cart features can be use where i need it
+ * on my application-
+ * 
+ * The 'active' class has the 'opacity' different to the
+ * 'color-btn' so can be easily identified as selected
+ * also the '<FaCheck' icon
+ * 
+ * in the next version i'll incorporate the increase and 
+ * decrease feature for the 'product' added to the cart
+*/
+
+const AddToCart = ({ product }) => {
+
+  /**here i destructure the 'product' props */
+  const { id, stock, colors } = product;
+
+  /**here i build the color state to handle the 'colors' 
+   * value */
+  const [ mainColor , setMainColor ] = useState(colors[0]);
+
+  return(
+      <Wrapper>
+        <div className='colors'>
+          {/**here i map the colors */}
+          <span> colors: </span>
+          <div>{
+            colors.map((color, index) => {
+              return <button 
+                style={{ background: color}}
+                key={index} 
+                className={`${mainColor === color ? 'color-btn active' : 'color-btn' }`}
+                onClick={() => setMainColor(color)}
+                >{ mainColor === color ? <FaCheck /> : null }</button>
+            })
+            }</div>
+        </div>
+      </Wrapper>
+    )
 }
 
 const Wrapper = styled.section`
