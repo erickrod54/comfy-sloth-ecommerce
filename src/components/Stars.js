9f3f1ae0d88769f1f54e7f2c8a077f84a105f5ed
Erick Rodriguez <erickrod54@gmail.com>
2022-07-12 11:35:42 -0400

    comfy-sloth-ecommerce app version 10 - Stars Component file - Features: Refactoring the stars - Programaticly version -

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,12 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 
-/**comfy-sloth-ecommerce app version 9 - Stars Component 
+/**comfy-sloth-ecommerce app version 10 - Stars Component 
  * file - Features: 
  * 
- *      --> Building the 'stars' -Manual approach- 
- * 
- *      --> Building the 'reviews'.             
+ *      --> Refactoring the 'stars' -Prograaticlly approach-              
  * 
  * Notes: For the stars will be three cases all of them covered 
  * by the ternary operator:
@@ -26,55 +24,34 @@ const Stars = ({ stars, reviews }) => {
   console.log('this is the stars value ==> ', stars ,
   'and this is the reviews value ==>', reviews)
 
+  /**i called 'tempStars' because i already use stars*/
+
+  /**i createa new empty array with length of 5 -five stars-*/
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    
+    /**the number is the rating value -1.5, 2.5, 3.5, 4.5- 
+     * now this way programaticly*/
+    const number = index + 0.5
+    return(
+      /**i fill the array with the star code, and i use the 
+       * number and index to modidy it -index + 1, cause my 
+       * first index is 0 - */
+      <span key={index}>
+          {stars >= index + 1 ? 
+          <BsStarFill /> : stars >= number ? 
+          <BsStarHalf /> 
+          : 
+          <BsStar />}
+        </span>
+    )
+  })
+  console.log('the tempStars ==>', tempStars)
+
   return(
     <Wrapper>
       {/** here i build the stars will be 5 -five-*/}
       <div className='stars'>
-        {/**star */}
-        <span>
-          {stars >= 1 ? 
-          <BsStarFill /> : stars >= 0.5 ? 
-          <BsStarHalf /> 
-          : 
-          <BsStar />}
-        </span>
-        {/**end of star */}
-        {/**star */}
-        <span>
-          {stars >= 2 ? 
-          <BsStarFill /> : stars >= 1.5 ? 
-          <BsStarHalf /> 
-          : 
-          <BsStar />}
-        </span>
-        {/**end of star */}
-        {/**star */}
-        <span>
-          {stars >= 3 ? 
-          <BsStarFill /> : stars >= 2.5 ? 
-          <BsStarHalf /> 
-          : 
-          <BsStar />}
-        </span>
-        {/**end of star */}
-        {/**star */}
-        <span>
-          {stars >= 4 ? 
-          <BsStarFill /> : stars >= 3.5 ? 
-          <BsStarHalf /> 
-          : 
-          <BsStar />}
-        </span>
-        {/**end of star */}
-        {/**star */}
-        <span>
-          {stars === 5 ? 
-          <BsStarFill /> : stars >= 4.5 ? 
-          <BsStarHalf /> 
-          : 
-          <BsStar />}
-        </span>
-        {/**end of star */}
+        {tempStars}
       </div>
       {/**here i set the reviews */}
       <p className='reviews'>({reviews} customer reviews)</p>
