6c4acb3871b818ce7d27ea878acf1a5b3178a490
Erick Rodriguez <erickrod54@gmail.com>
2022-07-15 12:09:16 -0400

    comfy-sloth-ecommerce app version 11 - ProductsPage file - Features: Importing and Placing PageHero, Filters, Sort and ProductList Components to build the ProductsPage -for this version is a starter-

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -2,8 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 import { Filters, ProductList, Sort, PageHero } from '../components'
 
+/**comfy-sloth-ecommerce app version 11 - ProductsPage
+ * file - Features: 
+ * 
+ *      --> Importing and Placing 'PageHero', 'Filters',
+ *          'Sort', and 'ProductList' Components to
+ *           build 'ProductsPage'                         
+ * 
+ * Notes: for this version will focus on building a 'ProductsList'
+ * and 'ProductsPage' starter 
+*/
+
+
 const ProductsPage = () => {
-  return <h4>products page</h4>
+  return(
+    <main>
+      <PageHero title='products' />
+      <Wrapper className='page'>
+        <div className='section-center products'>
+          <Filters />
+          <div>
+            <Sort />
+            <ProductList />
+          </div>
+        </div>
+      </Wrapper>
+    </main>
+
+  )
 }
 
 const Wrapper = styled.div`
