04be6ecce28486a64d0d9e3fc4cdc6fc7be5e0e1
Erick Rodriguez <erickrod54@gmail.com>
2022-07-15 12:15:52 -0400

    comfy-sloth-ecommerce app version 12 - ProductList Component - Features: Destructuring filtered_products from useFilterContext, Importing and Placing GridView Component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,8 +3,37 @@ import { useFilterContext } from '../context/filter_context'
 import GridView from './GridView'
 import ListView from './ListView'
 
+
+/**comfy-sloth-ecommerce app version 12 - ProductList
+ * Component - Features: 
+ * 
+ *      --> Destructuring 'filtered_products' from                                   
+ *          'useFilterContext()' 
+ * 
+ *       --> Importing and Placing 'GridView' 
+ *            Component.
+ * 
+ * Notes: As a starter will be set as 'GridView', but the final
+ * will have different views.
+ * 
+ * 'Gridview' Component will render the 'filtered_products' as a
+ * list
+*/
+
 const ProductList = () => {
-  return <h4>product list</h4>
+
+  /**here i destructure 'filtered_products' with a 'products' 
+   * allias */
+  const { filtered_products: products } = useFilterContext()
+
+  /**here i test that actually i can get the provided values */
+  //console.log('this is provided by FilterProvider ==>', products)
+
+  return(
+      <GridView products={products}>
+        product list
+      </GridView>
+    )
 }
 
 export default ProductList
