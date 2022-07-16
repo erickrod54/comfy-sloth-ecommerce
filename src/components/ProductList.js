4f56e3cc650d9703a3564c4b8071262646cb393f
Erick Rodriguez <erickrod54@gmail.com>
2022-07-16 18:37:06 -0400

    comfy-sloth-ecommerce app version 13 - ProductList Component - Features: Destructuring grid_view from useFilterContext(), Setting up a conditional rendering to render ListView Component depending on the grid_view value

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,31 +4,41 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 
-/**comfy-sloth-ecommerce app version 12 - ProductList
+/**comfy-sloth-ecommerce app version 13 - ProductList
  * Component - Features: 
  * 
- *      --> Destructuring 'filtered_products' from                                   
- *          'useFilterContext()' 
+ *      --> Destructuring 'grid_view' from 
+ *          'useFilterContext()'
  * 
- *       --> Importing and Placing 'GridView' 
- *            Component.
+ *      --> Setting up a conditional rendering
+ *          to render the 'ListView' Component
+ *          depending on 'grid_view' value.
  * 
- * Notes: As a starter will be set as 'GridView', but the final
- * will have different views.
- * 
- * 'Gridview' Component will render the 'filtered_products' as a
- * list
+ * Notes: There is an issue with the 'hot refresh'
+ * - react behavior-, so before setting the full 
+ * functionality, to test it out how the view changes, 
+ * i have to switch manually the 'grid_view' from 
+ * 'false' to 'true' and refresh the page.
 */
 
 const ProductList = () => {
 
-  /**here i destructure 'filtered_products' with a 'products' 
-   * allias */
-  const { filtered_products: products } = useFilterContext()
+  /**here i destructure 'grid_view' */
+  const { filtered_products: products, grid_view } = useFilterContext()
 
-  /**here i test that actually i can get the provided values */
-  //console.log('this is provided by FilterProvider ==>', products)
-
+  /**the condition to render if the product does
+   * not exist*/
+  if (products.length < 1) {
+    <h5 style={{ textTransform:'none'}}>
+      Sorry, no products match your search...
+    </h5>
+  }
+  /**If the grid view value is false, i render
+   * the 'ListView' Component*/
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
+  /**If any of above, i'll render 'GridView' Component */
   return(
       <GridView products={products}>
         product list
