f48c9e26d94548a8040bde202b865a42783c6c1c
Erick Rodriguez <erickrod54@gmail.com>
2022-07-17 17:52:30 -0400

    comfy-sloth-ecommerce app version 14 - filter_reducer file - Features: Building the action SET_GRIDVIEW, Building the action SET_LISTVIEW

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -9,22 +9,40 @@ import {
   CLEAR_FILTERS,
 } from '../actions'
 
-/**comfy-sloth-ecommerce app version 11 - filter_reducer
+/**comfy-sloth-ecommerce app version 14 - filter_reducer
  * file - Features: 
  * 
- *      --> Building the action LOAD_PRODUCTS                                 
+ *      --> Building the action 'SET_GRIDVIEW'.
  * 
- * Notes: the props that in the 'filter_context'
- * file were empty arrays initially, now they 
- * are going to be filled by the 'action.paylod'
- * - products - once the 'useEffect' is triggered
+ *      --> Building the action 'SET_LISTVIEW'.                                 
+ * 
+ * Notes: These actions must be build sequentially
+ * as the 'grid' triggers first - the icons order
+ * in Sort Component - it goes first and so on 
+ * with 'SET_LISTVIEW' action.
 */
 
 const filter_reducer = (state, action) => {
+  
   if (action.type === LOAD_PRODUCTS) {
       return {...state, all_products:[...action.payload], 
               filtered_products: [...action.payload]}    
   }
+
+  /**here i build the 'SET_GRIDVIEW' action*/
+  if (action.type === SET_GRIDVIEW) {
+    /**i spread/copy the state and switch the grid_value 
+     * to 'true'*/
+    return {...state, grid_view:true }
+  }
+
+  /**here i build the 'SET_LISTVIEW' action*/
+  if (action.type === SET_LISTVIEW) {
+    /**i spread/copy the state and switch the grid_value 
+     * to 'false'*/
+    return {...state, grid_view:false } 
+  }
+
   
   throw new Error(`No Matching "${action.type}" - action type`)
 }
