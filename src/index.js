44312385826d61f290660146e1022601890f927c
Erick Rodriguez <erickrod54@gmail.com>
2022-07-26 13:31:08 -0400

    comfy-sloth-ecommerce app version 26 - index js file - Features: Wrapping App using the CartProvider in order to provide products data, features and filters from ProductsProvider and FilterProvider to CartProvider

--- src/index.js
+++ src/index.js
@@ -8,26 +8,28 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-/**comfy-sloth-ecommerce app version 11 - index js file
+/**comfy-sloth-ecommerce app version 26 - index js file
  * - Features: 
  * 
- *      --> Wrapping 'App' using 'FilterProvider'
+ *      --> Wrapping 'App' using 'CartProvider'
  *          in order to provide products data and 
- *          features from 'ProductsProvider' to
- *          'FilterProvider'.
+ *          features from 'ProductsProvider' and
+ *          'FilterProvier' to 'CartProvider'.
  * 
  * Notes: the 'ProductsProvider' provides > FilterProvider and 
  * both provides all the '<App />'
  * 
  * There is an action 'LOAD_PRODUCTS' specially made with the 
  * propouse to provide the 'products' from 'ProductsProvider' 
- * to 'FilterProvider'
+ * to 'FilterProvider' 
 */
 
 ReactDOM.render(
 <ProductsProvider>
     <FilterProvider>
-        <App />
+        <CartProvider>
+            <App />
+        </CartProvider>
     </FilterProvider>
 </ProductsProvider>, 
 
