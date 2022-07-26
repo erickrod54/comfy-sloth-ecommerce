f59d4389e35c052d7476b0f054a3652f26e10151
Erick Rodriguez <erickrod54@gmail.com>
2022-07-26 13:28:45 -0400

    comfy-sloth-ecommerce app version - cart_context file - Features: Building the intialState, Building state to dispatch actions, Providing the state througt the provider

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,13 +8,37 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
-const initialState = {}
+/**comfy-sloth-ecommerce app version 26 - cart_context
+ * file - Features: 
+ * 
+ *      --> Building the 'initialState'.
+ * 
+ *      --> Building state to dispatch actions.
+ * 
+ *      --> Providing the state throught the 
+ *          provider. 
+ * 
+ * Notes: the values of the initial state they are
+ * going to be filled by the user interaction with 
+ * the cart, the shipping_fee value is in cents
+ * 
+*/
+
+/**adding the 'cart', 'total_items', 
+ * 'total_amount', 'shipping_fee' */
+const initialState = {
+  cart:[],
+  total_items:0,
+  total_amount:0,
+  shipping_fee:534,
+}
 
 const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
+  const [ state, dispatch ] = useReducer(reducer, initialState)
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{...state}}>{children}</CartContext.Provider>
   )
 }
 // make sure use
