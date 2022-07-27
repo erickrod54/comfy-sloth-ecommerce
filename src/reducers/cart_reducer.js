f6a0c65e622db2742ceb95f5f94175b3532be000
Erick Rodriguez <erickrod54@gmail.com>
2022-07-27 11:26:09 -0400

    comfy-sloth-ecommerce  app version 27 - cart_context file - Features: Building the ADD_TO_CART action -initial setup-

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -6,8 +6,38 @@ import {
   TOGGLE_CART_ITEM_AMOUNT,
 } from '../actions'
 
+/**comfy-sloth-ecommerce app version 27 - cart_context
+ * file - Features: 
+ * 
+ *      --> Building the 'ADD_TO_CART' action.
+ *          -initial setup-
+ * 
+ * Notes: This initial setup let me add the 'newItem'
+ * to the cart array and i can check on chrome > Components
+ * > CartProviver
+*/
+
 const cart_reducer = (state, action) => {
-  return state
+  if (action.type === 'ADD_TO_CART') {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((i) => i.id === id + color )
+
+    if (tempItem) {
+      
+    }else{
+      const newItem = {
+        id: id+color,
+        name: product.name,
+        color,
+        amount,
+        image:product.images[0].url,
+        price:product.price,
+        max:product.stock,
+
+      }
+      return {...state, cart:[...state.cart, newItem]}
+    }
+  }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
