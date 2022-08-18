91f0a65d468729215caafdf50f45b0ddc8b7ace8
Erick Rodriguez <erickrod54@gmail.com>
2022-08-18 01:16:50 -0400

    comfy-sloth-ecommerce app version 33 - CheckoutPage Component - Features: Connectiong CheckoutPage with StripeCheckout Component

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -5,24 +5,62 @@ import { PageHero, StripeCheckout } from '../components'
 import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
-/**comfy-sloth-ecommerce app version 2 - 'CheckoutPage'
+/**comfy-sloth-ecommerce app version 33 - 'CheckoutPage'
  * Component - Features: 
  * 
- *      --> Building basic 'Checkout' page.
+ *      --> Connecting 'CheckoutPage' with
+ *          'StripeCheckout' Component.
  * 
- * Notes: this basic 'Checkout' page is built in order
- * to test 'PageHero' Component
+ * Notes: the communication to proceed to payments will
+ * be as this: 
+ *  
+ *  --> 'CheckoutPage' > 'StripeCheckout'  
+ *      > 'functions/payments' > Stripe Servers
+ * 
+ * 'CheckoutPage' --> will verify based on the cart and
+ * render a message 'your cart is empty' or the 
+ * 'StripeCheckout' Component
+ * 
+ * 'StripeCheckout' --> will handle the payment info 
+ * with the stripe UI and communicate with a serveless
+ * function   
+ * 
+ * functions/payments' --> will handle the payment info
+ * to send it securely to 'Stripe Servers'
+ * 
+ * The communication must be this way because is extremily 
+ * dangerous to communicate directly with stripe - is
+ * exposing all the payments information-
+ * 
 */
-
 const CheckoutPage = () => {
+
+  const { cart } = useCartContext()
+
   return(
     <main>
       <PageHero title='checkout' />
       <Wrapper className='page'>
-        <h1>checkout here</h1>
+        { cart.length < 1 ? (
+          <div className='empty'>
+            <h2>Your cart is empty</h2>
+            <Link to='/products' className='btn'>
+              Fill it
+            </Link>
+          </div> 
+        ) :
+        (<StripeCheckout />)
+        }
       </Wrapper>
     </main>
     )
 }
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty{
+    text-align: center;
+  }
+`
 export default CheckoutPage
